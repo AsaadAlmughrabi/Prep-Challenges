26f715556c0b008148d4ac3368f698661282448b
AsaadAlmughrabi <assadalmograbe8@gmail.com>
2024-01-29 20:07:15 +0000

    resolved the challeng 09 in foreach instead of for loop

--- code-challenges/challenges-09.js
+++ code-challenges/challenges-09.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Important Note:
 // Kindly use forEach loop instead of for in all of your solutions
@@ -11,53 +11,51 @@
 // Required:
 
 //  Write a function that takes an array of numbers and increase the values by 10
-//  
+//
 //  Input:
 //  [20, 54, 89, 41]
 //  Output:
 //  [30, 64, 99, 51]
-//  
+//
 
 const arrInc = (arr) => {
-    let result = [];
-    // write your code here
-    for(let i=0;i<arr.length;i++)
-    {
-        result.push(arr[i] + 10);
-    }
-    return result;
-}
+  let result = [];
+  // write your code here
+  arr.forEach((element) => {
+    result.push(element + 10);
+  });
+  return result;
+};
 // -------------------------------------------------------------------------------------------------------
-
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 02:
 // Required:
 
-//  Write a function that takes an array of decimals and round every value to the closest value 
-//  
+//  Write a function that takes an array of decimals and round every value to the closest value
+//
 //  Input:
-//  [5.4, 5.5 ,6.7, 6.8] 
+//  [5.4, 5.5 ,6.7, 6.8]
 //  Output:
 //  [5, 6, 7, 7]
-// 
+//
 const roundDecimals = (arr) => {
-    // write your code here
-    for(let i=0;i<arr.length;i++)
-    {
-       arr[i]= Math.round(arr[i]);
-    }
-    return arr; 
-}
+  let result = [];
+  // write your code here
+  arr.forEach((element) => {
+    result.push(Math.round(element));
+  });
+  return result;
+};
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 03:
 // Required:
-// 
+//
 // An owner of a factory wants to give a 100$ bonus for production department employees who worked *More than* 8 hours
-// and 50$ for those who worked less, given their data increase their salary and return the data back again 
-//  
+// and 50$ for those who worked less, given their data increase their salary and return the data back again
+//
 // Input:
 // let data = [
 //     {
@@ -85,7 +83,7 @@ const roundDecimals = (arr) => {
 //         salary: "3200$"
 //     }
 // ]
-// 
+//
 // Output:
 // [
 //     {
@@ -116,16 +114,18 @@ const roundDecimals = (arr) => {
 //
 
 const employeesBonus = (arr) => {
-    // write your code here
-    for(let i=0;i<arr.length;i++){
-        if (arr[i].section === "Production" || arr[i].workHours > 8) {
-            arr[i].salary = parseInt(arr[i].salary) + 100 +"$";
+  // write your code here
+  for (let i = 0; i < arr.length; i++) {
+    arr.forEach((employee) => {
+        if (employee.section === "Production" || employee.workHours > 8) {
+          employee.salary = parseInt(employee.salary) + 100 + "$";
         } else {
-            arr[i].salary = parseInt(arr[i].salary) + 50 +"$";
+          employee.salary = parseInt(employee.salary) + 50 + "$";
         }
-    }
-    return arr;
+      });
+      return arr;
 }
+};
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
@@ -135,28 +135,27 @@ const employeesBonus = (arr) => {
 // Harry wants to buy a new mouse and keyboard for his new setup
 // He wants to choose one mouse and one keyboard from the list of prices and take the most expensive combination
 // but his budget is limited, help him by finding the most expensive *price* for a combination to buy with his budget
-// 
+//
 // Input:
 // budget = 200$
 // mouseArray = [35, 15, 75, 180, 150, 50]
 // keyBoardArray = [5, 150, 35, 120, 75, 50, 100]
-// 
+//
 // Output: 200
 
 const mostExpensive = (budget, mouseArray, keyBoardArray) => {
     // write your code here
-    let mostEx=0;
-    for(let i=0;i<mouseArray.length;i++){
-        for(let j=0;keyBoardArray.length;i++){
-                let combPrice=mouseArray[i]+keyBoardArray[j];
-                if(combPrice<=budget&& combPrice>mostEx){
-                mostEx=combPrice;
-    }
-}
-    }
+    let mostEx = 0;
+    mouseArray.forEach((mouse) => {
+      keyBoardArray.forEach((keyBoard) => {
+        let combPrice = mouse + keyBoard;
+        if (combPrice <= budget && combPrice > mostEx) {
+          mostEx = combPrice;
+        }
+      });
+    });
     return mostEx;
-}
-
+  };
 
 // -------------------------------------------------------------------------------------------------------
 
